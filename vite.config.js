8bd9f9f9762ddc6b63dfea5f4053d1ba86f8af0f
Andrej Buday <andrej.buday-ext@xitee.com>
2026-02-22 16:24:02 +0100

    2026 02 22 Missing main.jsx fix at codekopf.github.io

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cz-realie-test/',
+  base: '/cz-realie-tester/',
 })
